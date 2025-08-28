ccbede8bff420cbff0144d1285a15819808faec1
dameini <1029177123@qq.com>
2025-08-28 13:31:48 +0000

    0.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,13 @@
-
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+    }
+    plugins {
+        // 与下方 Gradle 8.7 兼容
+        id("com.android.application") version "8.5.2"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
     }
 }
 
